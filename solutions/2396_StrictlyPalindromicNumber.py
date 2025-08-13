a962f17e8cd85bf4514d229dc54ae5ed2e4f2be2
C0deCrusad3r <nirmala.ixc.hardik.runwal.7538@gmail.com>
2025-08-13 10:39:44 +0530

    Created 2396_StrictlyPalindromicNumber.py
    “Checks if a number is strictly palindromic by verifying it reads the same in all bases from 2 up to n-2.”

---
+++ solutions/2396_StrictlyPalindromicNumber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        def to_base(n, b):
+            res = ""
+            while n > 0:
+                res = str(n % b) + res
+                n //= b
+            return res
+        
+        for base in range(2, n - 1):
+            converted = to_base(n, base)
+            if converted != converted[::-1]:
+                return False
+        return True
