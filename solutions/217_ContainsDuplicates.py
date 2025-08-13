2a1f93ccc009c461289432b18c39caad4aa366f1
C0deCrusad3r <nirmala.ixc.hardik.runwal.7538@gmail.com>
2025-08-13 10:38:47 +0530

    Created 217_ContainsDuplicates.py
    “Determines if the input array contains any duplicate elements by comparing its length to the length of its set.”

---
+++ solutions/217_ContainsDuplicates.py
@@ -0,0 +1,11 @@
+#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if(len(set(nums))) < len(nums):
+            return True
+        else:
+            return False
